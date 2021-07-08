d55f00547cab6c76fe4adc26155917014c100e97
Documenter.jl <documenter@juliadocs.github.io>
2021-07-08 23:49:56 +0000

    build based on 1363fbf

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = IndirectTrajOpt","category":"page"},{"location":"#IndirectTrajOpt","page":"Home","title":"IndirectTrajOpt","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for IndirectTrajOpt.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [IndirectTrajOpt]","category":"page"}]
+}
